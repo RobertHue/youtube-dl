13b5581b4f3c0addd91caf9555ea7f43101ffa1c
Robert Hümmer <robert.huem@gmail.com>
2024-08-04 00:51:51 +0200

    added concurrent fragments capability

--- youtube-dl.py
+++ youtube-dl.py
@@ -19,13 +19,24 @@ ydl_video_best = {
 }
 
 
-def main(input_file, output_folder, strategy):
+def download_urls(urls, ydl_opts):
+    """Download multiple URLs using the specified yt_dlp options."""
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(urls)
+        logging.info("Successfully downloaded URLs")
+    except Exception as e:
+        logging.error(f"Error downloading URLs: {e}")
+
+
+def main(input_file, output_folder, strategy, concurrent_fragments):
     """Download videos or audios from a list of URLs based on the selected strategy.
 
     Args:
         input_file (str): Path to the text file containing the URLs.
         output_folder (str): Folder to save the downloaded videos or audios.
         strategy (str): The download strategy ('audio' or 'video').
+        concurrent_fragments (int): Number of concurrent fragments to use for downloading.
     """
     try:
         with open(input_file, "r") as file:
@@ -40,10 +51,11 @@ def main(input_file, output_folder, strategy):
     # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
-    ydl_opts = ydl_audio_m4a if strategy == "audio" else ydl_video_best
+    ydl_opts = ydl_audio_best if strategy == "audio" else ydl_video_best
+    ydl_opts["outtmpl"] = os.path.join(output_folder, "%(title)s.%(ext)s")
+    ydl_opts["concurrent_fragment_downloads"] = concurrent_fragments
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        error_code = ydl.download(urls)
+    download_urls(urls, ydl_opts)
 
 
 if __name__ == "__main__":
@@ -69,6 +81,12 @@ if __name__ == "__main__":
         default="audio",
         help="Download strategy: 'audio' to extract audio only, 'video' to download the best video with audio (default: audio)",
     )
+    parser.add_argument(
+        "--concurrent_fragments",
+        type=int,
+        default=4,
+        help="Number of concurrent fragments to use for downloading (default: 4)",
+    )
 
     args = parser.parse_args()
-    main(args.input_file, args.output_folder, args.strategy)
+    main(args.input_file, args.output_folder, args.strategy, args.concurrent_fragments)
